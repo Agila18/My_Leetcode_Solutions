7555729746f721a96a3e3e7383cb3ce6ef7de765
Agila18 <akilatamilselvan28.com>
2025-08-24 18:44:30 +0000

    Code challenge submission for question: 1342 Number of Steps to Reduce a Number to Zero

---
+++ 1342 Number of Steps to Reduce a Number to Zero/1342number-of-steps-to-reduce-a-number-to-zero.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numberOfSteps(int num) {
+        int step=0;
+        while(num!=0){
+            if((num & 1)==0)    num>>=1;
+            else        num--;
+            step++;
+        }
+        return step;
+    }
+}
